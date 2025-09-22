24485c19d0459ad03ec44bd75bc9fee386e463c1
janhavi73 <janhavisinghania0607@gmail.com>
2025-09-22 16:54:04 +0100

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,8 @@
+
+bill_amount =int(input("please enter the bill amount"))
+amount_paid =int(input("please enter the amount you paid"))
+
+
+change = amount_paid - bill_amount
+
+print("change is £",change )
